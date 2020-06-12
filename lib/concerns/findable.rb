04be06c86c8e709f08d364b2926f9f8a17f725ab
Grace Kim <goooyoon@gmail.com>
2020-06-12 05:19:22 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,17 @@
-module Concerns::Findable
+module Concerns
+  module Findable
+    def create(objectName)
+      object = self.new(objectName)
+      object.save
+      object
+    end
 
-  def find_by_name(name)
-    self.all.find {|s| s.name == name}
-  end 
+     def find_by_name(objectName)
+      self.all.find {|object| object.name == objectName }
+    end
 
-  def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
-  end 
-
-end 
+     def find_or_create_by_name(objectName)
+      self.find_by_name(objectName) ? self.find_by_name(objectName) : self.create(objectName)
+    end
+  end
+end
