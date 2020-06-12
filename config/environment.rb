795c9dc1aeee7e120f9bde3c5ee48ea94a8d52b0
Grace Kim <goooyoon@gmail.com>
2020-06-12 05:18:34 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,25 +1,4 @@
 
 Bundler.require
 
-module Concerns
-  module Findable
-    def find_by_name(name)
-      self.all.each do |x|
-        if x.name == name
-          return x
-        end
-      end
-      nil
-    end
-
-    def find_or_create_by_name(name)
-      if find_by_name(name)
-        return find_by_name(name)
-      else
-        return self.create(name)
-      end
-    end
-  end
-end
-
 require_all 'lib'
